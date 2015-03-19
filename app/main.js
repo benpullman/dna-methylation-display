aaff2019647b86f6db31647732d3473a6e366300
irenelfeng <irene.l.feng.17@dartmouth.edu>
2015-03-19 09:49:43 -0400

    toggle switch for lines and dots

--- app/main.js
+++ app/main.js
@@ -9,12 +9,13 @@ require('factories/methylation');
 require('factories/loadData');
 require('filters/markdown');
 require('directives/dot');
+require('directives/block');
 
 // Declare app level module which depends on filters, and services
 angular.module('app', ['ngRoute','ngCookies','ui.bootstrap',
 	'app.controllers','app.submitController','angularFileUpload','app.userController','app.viewController',
 	'app.methylation', 'app.loadData',
-	'app.services.resource','app.services.user','markdown','dotDirective']).
+	'app.services.resource','app.services.user','markdown','dotDirective','blockDirective']).
 config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/welcome.html', controller: 'UserController'});
   $routeProvider.when('/view/:id', {templateUrl: 'partials/main.html', controller: 'MainController'});
