d2f6e9587e95595409b716339a9fa07692d599bf
irenelfeng <irene.l.feng.17@dartmouth.edu>
2015-03-24 11:23:09 -0400

    add reroute for view

--- app/main.js
+++ app/main.js
@@ -12,17 +12,18 @@ require('filters/nameExp');
 require('filters/markdown');
 require('directives/dot');
 require('directives/block');
+require('directives/methylationChart')
 
 // Declare app level module which depends on filters, and services
 angular.module('app', ['ngRoute','ngCookies','ui.bootstrap',
 	'app.controllers','app.submitController','angularFileUpload','app.userController','app.viewController',
 	'app.testController',
 	'app.methylation', 'app.loadData',
-	'app.services.resource','app.services.user','nameExp','markdown','dotDirective','blockDirective']).
+	'app.services.resource','app.services.user','nameExp','markdown','dotDirective','blockDirective','methylationChart']).
 config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/welcome.html', controller: 'UserController'});
   $routeProvider.when('/view/:id', {templateUrl: 'partials/main.html', controller: 'MainController'});
-  $routeProvider.when('/test', {templateUrl: 'partials/view.html', controller: 'ViewController'});
-  $routeProvider.when('/testing', {templateUrl: 'partials/timeline.html', controller: 'TestController'});
+  $routeProvider.when('/view/:id/sample/:sample', {templateUrl: 'partials/view.html', controller: 'ViewController'});
+  // $routeProvider.when('/test', {templateUrl: 'partials/view.html', controller: 'ViewController'});
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
